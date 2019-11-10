f7a9bd0b637044821f88ec00329e43877fe00d34
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-11-10 22:34:54 +0800

    added layout and css.

--- src/Campaigns/Campaigns.jsx
+++ src/Campaigns/Campaigns.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
+import { Layout } from 'antd'
 
 import useCampaignReducer from './useCampaignReducer'
 import useDispatchGlobalEffect from './useDispatchGlobalEffect'
 import CampaignFilter from './CampaignFilter'
+import styles from './Layout.module.css'
+
+const { Header, Content } = Layout
 
 const Campaigns = () => {
   const [campaigns, dispatch] = useCampaignReducer()
   useDispatchGlobalEffect(dispatch)
 
   return (
-    <div>
-      <h1>Campaigns</h1>
-      <CampaignFilter
-        campaigns={campaigns}
-      />
+    <div className={styles.layout}>
+      <Layout>
+        <Header>
+          <h1>
+            Campaigns
+          </h1>
+        </Header>
+        <Content>
+          <CampaignFilter
+            campaigns={campaigns}
+          />
+        </Content>
+      </Layout>
     </div>
   )
 }
