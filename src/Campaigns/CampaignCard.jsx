a1aa9763d65067336c6af645bbb343a37a03c89d
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-11-10 23:23:41 +0800

    make card look nicer.

--- src/Campaigns/CampaignCard.jsx
+++ src/Campaigns/CampaignCard.jsx
@@ -3,40 +3,43 @@ import PropTypes from 'prop-types'
 import { Card, Row, Col, Typography } from 'antd'
 
 import CampaignActiveTag from './CampaignActiveTag'
+import styles from './CampaignCard.module.css'
 
 const CampaignListItem = (props) => {
   const { campaign } = props
 
   return (
-    <Card>
-      <Row
-        type='flex'
-        justify='space-between'
-        align='middle'
-        style={{ marginBottom: '12px', height: '24px' }}
-      >
-        <Col>
-          <Typography.Text>
-            {campaign.startDate} - {campaign.endDate}
-          </Typography.Text>
-        </Col>
-        <Col>
-          <CampaignActiveTag campaign={campaign} />
-        </Col>
-      </Row>
-      <h3>
-        <Typography.Text
-          ellipsis
+    <div className={styles.campaignCard}>
+      <Card>
+        <Row
+          type='flex'
+          justify='space-between'
+          align='middle'
+          style={{ marginBottom: '12px', height: '24px' }}
         >
-          {campaign.name}
+          <Col>
+            <Typography.Text>
+              {campaign.startDate} - {campaign.endDate}
+            </Typography.Text>
+          </Col>
+          <Col>
+            <CampaignActiveTag campaign={campaign} />
+          </Col>
+        </Row>
+        <h3>
+          <Typography.Text
+            ellipsis
+          >
+            {campaign.name}
+          </Typography.Text>
+        </h3>
+        <Typography.Text
+          type='secondary'
+        >
+          {campaign.Budget} USD
         </Typography.Text>
-      </h3>
-      <Typography.Text
-        type='secondary'
-      >
-        {campaign.Budget} USD
-      </Typography.Text>
-    </Card>
+      </Card>
+    </div>
   )
 }
 
