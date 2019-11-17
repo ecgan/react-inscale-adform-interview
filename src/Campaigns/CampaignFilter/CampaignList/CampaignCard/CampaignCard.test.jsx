e1481842fe39540a5d0a46fc7380d54a924274df
Gan Eng Chin <ecgan@users.noreply.github.com>
2019-11-17 22:05:45 +0800

    added tests for rendering Active tag in CampaignCard.

--- src/Campaigns/CampaignFilter/CampaignList/CampaignCard/CampaignCard.test.jsx
+++ src/Campaigns/CampaignFilter/CampaignList/CampaignCard/CampaignCard.test.jsx
@@ -44,6 +44,44 @@ test('Campaign card should render dates in "startdate - enddate" format', () => 
   expect(node).not.toBeNull()
 })
 
+test('Active campaign card should render Active tag', () => {
+  const campaign = {
+    id: 1,
+    name: 'Forever Campaign',
+    startDate: '1/1/1970',
+    endDate: '1/1/2999',
+    Budget: 88377
+  }
+  const { queryByText } = render(
+    <CampaignCard
+      campaign={campaign}
+    />
+  )
+
+  const node = queryByText('Active')
+
+  expect(node).toBeInTheDocument()
+})
+
+test('Inactive campaign card should not render Active tag', () => {
+  const campaign = {
+    id: 1,
+    name: 'Old Campaign',
+    startDate: '1/1/1970',
+    endDate: '1/1/1979',
+    Budget: 88377
+  }
+  const { queryByText } = render(
+    <CampaignCard
+      campaign={campaign}
+    />
+  )
+
+  const node = queryByText('Active')
+
+  expect(node).not.toBeInTheDocument()
+})
+
 test('Campaign card should render abbreviated budget with USD suffix', () => {
   const campaign = {
     id: 1,
